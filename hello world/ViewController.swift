e0c8b9c9bbf4a0ff8c01aa113cf3d862ac5ed24e
xcode <xcode@sce11-w03.southhills.edu>
2017-12-11 15:11:52 -0500

    first commit

--- hello world/ViewController.swift
+++ hello world/ViewController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet var lable1: UILabel!
+    
+    @IBOutlet var input: UITextField!
+    
+    @IBOutlet var delete: UIButton!
+    
+    @IBAction func action(_ sender: UIButton) {
+        lable1.text = "Hello, " + (input.text ?? "")
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        lable1.text = ""
+        input.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        lable1.text = ""
         // Do any additional setup after loading the view, typically from a nib.
     }
 
